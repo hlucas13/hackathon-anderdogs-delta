7cb276a7da221ffad6657ba3f2ab03703e815f26
ll-catarino <ll.catarino@campus.fct.unl.pt>
2020-12-09 20:03:19 +0000

    api add user, return users json

---
+++ src/routes/users.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+router.route('/').get((req, res) => {
+    User.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    const newUser = new User({username, email, password, name});
+
+    newUser.save()
+        .then(() => res.json('User added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+module.exports = router;
